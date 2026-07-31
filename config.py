730b5a1826475af37898393f5364e8058d6d9bed
Jairam88 <87987267+Jairam88@users.noreply.github.com>
2026-07-31 14:10:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-API_ID = 30918158 
-API_HASH = "795178cb0ef1cc68690b1bbe82960214" 
-BOT_TOKEN = "7999558903:AAFmnpddylgWzlofbslPYtviziARBYya-i0" 
+import os
+
+API_ID = int(os.environ.get("30918158", "0"))
+API_HASH = os.environ.get("795178cb0ef1cc68690b1bbe82960214", "")
+BOT_TOKEN = os.environ.get("7999558903:AAFmnpddylgWzlofbslPYtviziARBYya-i0", "")
